68a90047397c679ce0e852cb52546f9652994690
Lonny Chen <lonny.cl.chen@gmail.com>
2023-08-14 20:07:11 +0200

    04-dashboard school DataTables updates

--- notebooks/dashboard_config.py
+++ notebooks/dashboard_config.py
@@ -10,9 +10,13 @@
 
 #ALL TABS
 initial_zoom = 4
-color_values = ['average_300', 'pct_passed', 'PTR', 'PBR_std7', 'BPR_std7', 'CG_per_student', 'approx_ages_mean', 'pop_3km', 'd_closest', 'd_council_hq', 'average_5tile', 'average_5tile_filtered']
+school_map_zoom = 17
+color_values = ['average_300', 'grade', 'pct_passed', 'average_5tile', 'average_5tile_filtered', 'PTR', 'PBR_std7', 'BPR_std7', 'CG_per_student', 'approx_ages_mean', 'pop_3km', 'd_closest', 'd_council_hq']
 color_labels = {'average_300': 'PSLE average (total 300)',
+                'grade': 'Grade (A-E)', #category_orders
                 'pct_passed': 'PSLE % passed (A-C)',
+                'average_5tile': 'PSLE quintile (national)', #category_orders
+                'average_5tile_filtered': 'PSLE quintile (filtered)', #category_orders
                 'PTR': 'Pupil-to-Teacher Ratio (PTR)',
                 'PBR_std7': 'Pupil-to-Book Ratio (PBR) (Std 7)',
                 'BPR_std7': 'Book-to-Pupil Ratio (BPR) (Std 7)',
@@ -20,35 +24,29 @@ color_labels = {'average_300': 'PSLE average (total 300)',
                 'approx_ages_mean': 'Ages mean (approx.)',
                 'pop_3km': 'Population within 3km radius',
                 'd_closest': 'Distance to closest other gov. school (km)',
-                'd_council_hq': 'Distance to council headquarters (km)',
-                'average_5tile': 'PSLE quintile (national)',
-                'average_5tile_filtered': 'PSLE quintile (filtered)'}
+                'd_council_hq': 'Distance to council headquarters (km)'}
 category_orders = {'average_5tile': ['highest', 'fourth', 'middle', 'second', 'lowest'],
-                   'average_5tile_filtered': ['highest', 'fourth', 'middle', 'second', 'lowest']}
-
+                   'average_5tile_filtered': ['highest', 'fourth', 'middle', 'second', 'lowest'],
+                   'grade': ['A', 'B', 'C', 'D', 'E']}
+tamisemi_url = 'https://www.tamisemi.go.tz/singleministers/basic-education-data-2022'
 #REVIEW
 labels = {'average_300': 'PSLE average', 'average_5tile': 'PSLE quintile', 'n_schools': 'Number of schools'}
 
 #PER-TAB LISTS
 #Hover data input to maps: what gets seen during mouse hover
-#Custom data input to maps: additional data for maps' `clickData` input to table
 hover_data = list()
+#Custom data input to maps: additional data for maps' `clickData` input to table
 custom_data = list()
-custom_data_DT = list()
 
 #SCHOOLS TAB [0]
 #Plotting/table data inputs
 hover_data.append({'LATITUDE fix': False, 'LONGITUDE fix': False, 'average_300': ':.2f'})
-custom_data.append(['school_name', 'SCHOOL OWNERSHIP', 'TOTAL STUDENTS', #school info [0:3]
-               'region_name', 'council_name', 'context', 'WARD', #location [3:7]
-               'num_sitters', 'average_5tile', 'average_300', 'grade', 'pct_passed', 'results_url', #PSLE results [7:13]
-               'PTR', 'PBR_std7', 'CG_per_student', #Resources [13:16]
-               'approx_ages_mean', 'pop_3km', 'd_closest', 'd_council_hq']) #demographic/geographic [16:19]
-custom_data_DT.append(['PRIMARY SCHOOL NAME', 'Ownership', 'Total students',
-                  'Region', 'Council', 'Context', 'Ward',
-                  'PSLE sitters', 'PSLE quintile', 'PSLE average (300)', 'PSLE average (grade)', 'PSLE % passed (A-C)', 'PSLE results URL',
-                  'Pupil-to-Teacher Ratio (PTR)', 'Pupil-to-Book Ratio (PBR) (Std 7)', 'Capitation Grant (CG) per student (TZS)',
-                  'Ages mean (approx.)', 'Population within 3km radius', 'Distance to closest other gov. school (km)', 'Distance to council headquarters (km)'])
+custom_data.append(['school_name', 'school_id', #title [0:2]
+                    'WARD', 'council_name', 'region_name', #location[2:5]
+                    'SCHOOL OWNERSHIP', 'TOTAL STUDENTS', #info [5:7]
+                    'results_url', 'num_sitters', 'average_300', 'grade', 'pct_passed', #PSLE results [7:12]
+                    'PTR', 'PBR_std7', 'BPR_std7', 'CG_per_student', 'approx_ages_mean', #TAMISEMI [12:17]
+                    'LATITUDE fix', 'LONGITUDE fix', 'pop_3km', 'd_closest', 'd_council_hq']) #demographic/geographic [17:22]
 
 #REGIONS TAB [1]
 #Plotting/table data inputs
@@ -57,7 +55,4 @@ custom_data.append(['region_name', 'schools_n', 'councils_n', 'students_sum', #r
                'sitters_sum', 'average_300', 'pct_passed', #PSLE results [4:6]
                'PTR', 'PBR_std7', 'CG_per_student', #Resources [7:9]
                'approx_ages_mean', 'pop_3km', 'd_closest', 'd_council_hq']) #Demographics/Geography (Xd) [10:13]
-custom_data_DT.append(['REGION', 'Number of schools', 'Number of councils', 'Total students (region)',
-                  'PSLE sitters (region)', 'PSLE average (300)', 'PSLE % passed (A-C)',
-                  'Pupil-to-Teacher Ratio (PTR)', 'Pupil-to-Book Ratio (PBR) (Std 7)', 'Capitation Grant (CG) per student (TZS)',
-                  'Ages mean (approx.)', 'Population within 3km radius', 'Distance to closest other gov. school (km)', 'Distance to council headquarters (km)'])
+
--- notebooks/dashboard_utils.py
+++ notebooks/dashboard_utils.py
@@ -5,6 +5,9 @@
 #Data handling
 import numpy as np
 import pandas as pd
+
+#Strings
+import re
 ####################
 
 def filter_df_or_all(df, col, sel_values, all_value='ALL DEFAULT'):
@@ -13,3 +16,16 @@ def filter_df_or_all(df, col, sel_values, all_value='ALL DEFAULT'):
         return df
     else:
         return df[df[col].isin(sel_values)]
+
+def convert_council_name(council_name):
+    '''Converts council name based on suffix'''
+    m = re.search('(.*)\s+(TC|MC|CC)', council_name)
+    if m != None:
+        if m[2] == 'TC':
+            return f'{m[1]} Town'
+        elif m[2] == 'MC':
+            return f'{m[1]} Municipality'
+        elif m[2] == 'CC':
+            return f'{m[1]} City'
+    else:
+        return f'{council_name} District'
