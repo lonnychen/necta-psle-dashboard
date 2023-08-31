9e2b603d9dee5846c208aafd8177a5c37767623f
Lonny Chen <lonny.cl.chen@gmail.com>
2023-08-31 09:30:39 +0200

    Cleanup Regions tab to match Schools tab layout and controls

--- notebooks/dashboard_config.py
+++ notebooks/dashboard_config.py
@@ -8,9 +8,9 @@
 #Plotting/table DATA inputs
 ###########################
 
-#ALL TABS
 initial_zoom = 4
 school_map_zoom = 17
+region_map_zoom = 8
 color_values = ['average_300', 'grade', 'pct_passed', 'average_5tile', 'average_5tile_filtered', 'PTR', 'PBR_std7', 'BPR_std7', 'CG_per_student', 'approx_ages_mean', 'pop_3km', 'd_closest', 'd_council_hq']
 color_labels = {'average_300': 'PSLE average (total 300)',
                 'grade': 'Grade (A-E)', #category_orders
@@ -29,16 +29,17 @@ category_orders = {'average_5tile': ['highest', 'fourth', 'middle', 'second', 'l
                    'average_5tile_filtered': ['highest', 'fourth', 'middle', 'second', 'lowest'],
                    'grade': ['A', 'B', 'C', 'D', 'E']}
 tamisemi_url = 'https://www.tamisemi.go.tz/singleministers/basic-education-data-2022'
+
 #REVIEW
 labels = {'average_300': 'PSLE average', 'average_5tile': 'PSLE quintile', 'n_schools': 'Number of schools'}
 
-#PER-TAB LISTS
+#PER-MAP TRACE LISTS
 #Hover data input to maps: what gets seen during mouse hover
 hover_data = list()
 #Custom data input to maps: additional data for maps' `clickData` input to table
 custom_data = list()
 
-#SCHOOLS TAB [0]
+#SCHOOLS TRACE [0]
 #Plotting/table data inputs
 hover_data.append({'LATITUDE fix': False, 'LONGITUDE fix': False, 'average_300': ':.2f'})
 custom_data.append(['school_name', 'school_id', #title [0:2]
@@ -48,11 +49,11 @@ custom_data.append(['school_name', 'school_id', #title [0:2]
                     'PTR', 'PBR_std7', 'BPR_std7', 'CG_per_student', 'approx_ages_mean', #TAMISEMI [13:18]
                     'LATITUDE fix', 'LONGITUDE fix', 'pop_3km', 'd_closest', 'd_council_hq']) #demographic/geographic [18:23]
 
-#REGIONS TAB [1]
+#REGIONS TRACE [1]
 #Plotting/table data inputs
-hover_data.append({'region_name': False, 'schools_n': True, 'average_300': ':.2f'})
-custom_data.append(['region_name', 'schools_n', 'councils_n', 'students_sum', #region info [0:3]
-               'sitters_sum', 'average_300', 'pct_passed', #PSLE results [4:6]
-               'PTR', 'PBR_std7', 'CG_per_student', #Resources [7:9]
-               'approx_ages_mean', 'pop_3km', 'd_closest', 'd_council_hq']) #Demographics/Geography (Xd) [10:13]
-
+hover_data.append({'region_name': False, 'num_schools': True, 'average_300': ':.2f'})
+custom_data.append(['region_name_full', #title [0]
+                    'num_councils', 'num_schools', 'TOTAL_STUDENTS', #region info [1:4]
+                    'results_url', 'num_sitters', 'num_passed', 'average_300', 'grade', 'pct_passed', #PSLE results [4:10]
+                    'PTR', 'PBR_std7', 'BPR_std7', 'CG_per_student', 'approx_ages_mean', #TAMISEMI [10:15]
+                    'map_url', 'pop_3km', 'd_closest', 'd_council_hq']) #Demographics/Geography (Xd) [15:19]
